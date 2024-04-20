2b8d1f841766fb9975ebf566e0706a50e9332159
Viateur <akimanaviateur94@gmail.com>
2024-04-20 21:55:33 +0200

    customizing radix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme } from '@radix-ui/themes';
 import Navbar from "./Navbar";
+import {ThemePanel} from '@radix-ui/themes'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme>
+    
+        <Theme accentColor="iris">
         <Navbar />
         <main className='m-5'> {children}</main>
+       
         </Theme>
       </body>
     </html>
