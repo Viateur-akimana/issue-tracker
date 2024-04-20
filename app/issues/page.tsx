e4424ad523b04a9041031dc916ab1e66ec588ba4
Viateur <akimanaviateur94@gmail.com>
2024-04-20 16:15:57 +0200

    Getting and setting up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes';
 
 const IssuePage = () => {
   return (
-    <div>IssuePage</div>
+    <div><Button>New issue</Button></div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import '@radix-ui/themes/styles.css';
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Theme } from '@radix-ui/themes';
 import Navbar from "./Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Theme>
         <Navbar />
         <main> {children}</main>
+        </Theme>
       </body>
     </html>
   );
