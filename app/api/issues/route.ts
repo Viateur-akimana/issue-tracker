17f4a5b228ccaf05c1c0661a89ea933a25408690
Viateur <akimanaviateur94@gmail.com>
2024-04-20 16:08:57 +0200

    building api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod';
+import prisma from "@/prisma/client";
+const schema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)
+})
+
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = await schema.safeParse(body);
+
+    if (!validation.success) {
+        return NextResponse.json(validation.error.errors);
+    }
+
+    const newIssue = await prisma.issue.create({ data: { title: body.title, description: body.description } })
+    return NextResponse.json(newIssue,{status: 201});
+
+}
