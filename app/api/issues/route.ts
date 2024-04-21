1c640d195ab41a575345d8af2e0fb0211f95f74a
Viateur <akimanaviateur94@gmail.com>
2024-04-21 16:21:09 +0200

    Handling error

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,17 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
 import prisma from "@/prisma/client";
 const schema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1,'Title is required').max(255),
+    description: z.string().min(1,'Description is required')
 })
 
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    const validation = await schema.safeParse(body);
+    const validation = await schema.safeParse(body);    
 
     if (!validation.success) {
-        return NextResponse.json(validation.error.errors);
+        return NextResponse.json(validation.error.format());
     }
 
     const newIssue = await prisma.issue.create({ data: { title: body.title, description: body.description } })
