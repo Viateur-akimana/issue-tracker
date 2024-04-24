e9523311451b50f7225298f2e8b8e42d0b538f30
viateur <akimanaviateur94@gmail.com>
2024-04-24 21:10:58 +0200

    Implementing adding new issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
+import Image from 'next/image'
+import Icon from '../../../public/Icon.png'
+import Group from '../../../public/Group.png'
 
 const IssuesPage = () => {
-  return (
-    <div>
-        <h1>Streamlined Workflow</h1>
-        <p>Effortless issue management.</p>
-    </div>
-  )
+    return (
+        <main className='container mx-auto flex justify-center items-center h-screen'>
+            <h1>Streamlined Workflow</h1>
+            <p>Effortless issue management.</p>
+            <div className=''>
+                <div>
+                    <div className='item-center'>
+                        <Image src={Icon} alt={'Issues'} />
+                        <div>
+                            <h1>Create Issues</h1>
+                            <p>Report new issues with ease using our intuitive interface.</p>
+                        </div>
+
+                    </div>
+                    <div>
+                        <Image src={Icon} alt={'Issues'} />
+                        <div>
+                            <h1>Create Issues</h1>
+                            <p>Report new issues with ease using our intuitive interface.</p>
+                        </div>
+                    </div>
+                    <div>
+                        <Image src={Icon} alt={'Issues'} />
+                        <div>
+                            <h1>Create Issues</h1>
+                            <p>Report new issues with ease using our intuitive interface.</p>
+                        </div>
+                    </div>
+                </div>
+                <Image src={Group} alt={'Phone'}
+                />
+            </div>
+
+        </main>
+    )
 }
 
 export default IssuesPage
