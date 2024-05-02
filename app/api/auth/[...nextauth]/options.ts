948a3c12071e8a825e963ebc2a814f1552921abb
viateur <akimanaviateur94@gmail.com>
2024-05-02 08:01:23 +0200

    github handling

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -3,12 +3,37 @@ import GitHubProvider from 'next-auth/providers/github'
 import Credentials from "next-auth/providers/credentials";
 
 
-export const options: NextAuthOptions ={
-    providers:[
+export const options: NextAuthOptions = {
+    providers: [
         GitHubProvider({
-            clientId:process.env.GITHUB_ID as string,
-            clientSecret:process.env.GITHUB_SECRET as string,
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        Credentials({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-cool-username"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-awesome-password"
+                }
+            },
+            async authorize(credentials) {
+                const user = { id: "42", name: "Dave", password: "nextauth" }
+
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                } else {
+                    return null
+                }
+            }
         })
     ],
+
 
 }
